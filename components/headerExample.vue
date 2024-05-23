86c26b274e85a4d5c98077744696b2a7d1b1fea3
Lyrt1337 <122387064+Lyrt1337@users.noreply.github.com>
2024-05-23 16:25:40 +0200

    first test

--- components/headerExample.vue
+++ components/headerExample.vue
@@ -3,6 +3,16 @@
     <template #header>
       <div id="header-inner-container">
         <h1>Welcome to FHGR Frontend Development Lectures</h1>
+        <div class="logo">
+            <a href="index.html">
+                <img src="img/home_logo_green.jpg" width="150px" height="150px" alt="Student Homes">
+            </a>
+        </div>
+        <div>
+          <a href="login.html">
+            <img src="img/login_gray.png" width="45px" height="45px">
+          </a>
+        </div>
         <ColorScheme>
           <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']"/>
         </ColorScheme>
