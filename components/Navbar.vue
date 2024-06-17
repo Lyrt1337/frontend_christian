c7617b20e63ebfb32ae996d5d0250abb3b1f7b7d
Lyrt1337 <122387064+Lyrt1337@users.noreply.github.com>
2024-06-17 20:49:54 +0200

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,26 +4,20 @@
         <div id="nav-inner-container">
           <!-- <h1>Welcome to FHGR Frontend Development Lectures</h1> -->
         
-        <div>
-            <nav>
-                <ul id="nav_links">
-                    <li><NuxtLink to="/">Übersicht</NuxtLink></li>
-                    <li><NuxtLink to="/page/Städte Erkunden">Städte Erkunden</NuxtLink></li>
-                    <li><NuxtLink to="/page/Inserat Erstellen">Inserat Erstellen</NuxtLink></li>
-                    <li><NuxtLink to="/page/Info">Info</NuxtLink></li>
-                    <li><NuxtLink to="/page/Kontakt">Kontakt</NuxtLink></li>
-                </ul>
-            </nav>
+
+        <nav>
+            <ul class="nav_links">
+                <li><NuxtLink to="/">Übersicht</NuxtLink></li>
+                <li><NuxtLink to="/page/Städte Erkunden">Städte Erkunden</NuxtLink></li>
+                <li><NuxtLink to="/page/Inserat Erstellen">Inserat Erstellen</NuxtLink></li>
+                <li><NuxtLink to="/page/Info">Info</NuxtLink></li>
+                <li><NuxtLink to="/page/Kontakt">Kontakt</NuxtLink></li>
+            </ul>
+        </nav>
+      
         </div>
-            
-        </div>
-          <!-- <ColorScheme>
-            <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']"/>
-          </ColorScheme> -->
-        <!-- </div> -->
       </template>
-      <!-- <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation
-      </UButton> -->
+
     </UCard>
   </template>
   
@@ -34,36 +28,35 @@
   <style scoped lang="css">
   #nav-container {
     margin-top: 32px;
-  }
-  
-  #nav-inner-container {
-    justify-content: right;
-    display: flex;
-  }
+    }
+    
+    #nav-inner-container {
+        justify-content: right;
+        display: flex;
+    }
 
 
-  #nav_links{
-  display:flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  list-style:none;
-  padding: 0;
+    .nav_links{
+        display:flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        list-style:none;
+        padding: 0;
+        margin:0px;
+    }
 
-  margin:0px;
-}
-
-#nav_links li a{
-  display: block;
-  box-sizing:border-box;
-  height:40px;
-  padding: 10px 15px 0px 15px;
-}
+    .nav_links li a{
+        display: block;
+        box-sizing:border-box;
+        height:40px;
+        padding: 10px 15px 0px 15px;
+    }
 
 
-#nav_links li a:hover, .aktiver_menuelink{
-  color:blue;
-  border-bottom-style: solid;
-  border-bottom-width: 5px;
-  border-bottom-color: blue;
-}
+    .nav_links li a:hover{
+        color:blue;
+        border-bottom-style: solid;
+        border-bottom-width: 5px;
+        border-bottom-color: blue;
+    }
   </style>
